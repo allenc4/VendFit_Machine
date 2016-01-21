34296c42dde4f88f1432167a68a3def41d1f38ad
Tyler <tyler@digitalinsurancegroup.com>
2016-01-21 17:02:18 -0500

    Updates to process opcodes and 'vend'

--- main.cpp
+++ main.cpp
@@ -1,41 +1,110 @@
 #include "main.h"
 
+#define NUMBER_OF_SERVOS 4
+#define NUMBER_OF_BYTES 8
+
 Servo s1;
 Servo s2;
 Servo s3;
 Servo s4;
-         
 
-int pos = 0;
+Servo servos[NUMBER_OF_SERVOS];
+         
+int currentIndex = 0;
+
+char opcode[NUMBER_OF_BYTES];
+char incomingByte = 0;
+bool processing = false;
+
+
+void vend(int servo);
+
 
 void setup() 
 { 
 	Serial.begin(9600);
-  	s1.attach(A0);  
-  	s2.attach(A1);  
-  	s3.attach(A4);  
-  	s4.attach(A5);  
+  servos[0].attach(A0);
+  servos[1].attach(A1);
+  servos[2].attach(A4);
+  servos[3].attach(A5);
+
+  for(int i = 0; i < NUMBER_OF_SERVOS; i++)
+  {
+      servos[i].write(180);
+  }
+
+  Serial.println("Setup complete");
 } 
 
 
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos++)  
-  {                               
-    s1.write(pos);  
-    s2.write(pos);  
-    s3.write(pos);  
-    s4.write(pos);  
-    Serial.println(pos);          
-    delay(15);                    
-  } 
-  for(pos = 180; pos>0; pos--)    
-  {                                
-    s1.write(pos);  
-    s2.write(pos);  
-    s3.write(pos);  
-    s4.write(pos);             
-    Serial.println(pos);
-    delay(15);                    
-  } 
+  do{
+      if(Serial.available() > 0 && processing == false)
+     {
+        incomingByte = Serial.read();
+
+        Serial.print("Received: ");
+        Serial.println(incomingByte);
+
+        if(currentIndex > NUMBER_OF_BYTES)
+        {
+            Serial.print("ERROR: Invalid code.  Code Length max (");
+            Serial.print(NUMBER_OF_BYTES);
+            Serial.println(")");
+            currentIndex = 0;
+        }else
+        {
+            if(incomingByte == 10)
+            {
+              break;
+            }else
+            {
+                opcode[currentIndex] = incomingByte;
+                currentIndex++;
+            }
+        }
+     }
+  }while(incomingByte != 10);
+
+  incomingByte = 0;
+  currentIndex = 0;
+  processing = true;
+
+  for(int i = 0; i < NUMBER_OF_BYTES; i++)
+  {
+      if(opcode[i] == 10)
+      {
+        break;
+      }
+      
+      Serial.print(opcode[i]);
+  }
+
+  switch(opcode[0])
+  {
+
+      case 'v':
+          vend(opcode[1]-48); //convert from ascii to decimal 48 is ascii for 0
+          processing = false;
+        break;
+      default:
+        Serial.print("ERROR: Invalid opcode (");
+        Serial.print(opcode[0]);
+        Serial.println(")");
+        processing = false;
+        break;
+
+  }
+
+}
+
+void vend(int servo)
+{
+    if(servo > 0 && servo < NUMBER_OF_SERVOS + 1)
+    {
+        servos[servo-1].write(0);
+        delay(1000);
+        servos[servo-1].write(180);
+    }
 }
